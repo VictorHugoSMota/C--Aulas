04de9e486234f3bcae5e06405f653150c6fbfe34
Victor Hugo <victorhugo.mota26@gmail.com>
2026-02-10 20:02:05 -0300

    feat method for

--- Aulas/Program.cs
+++ Aulas/Program.cs
@@ -189,6 +189,10 @@ class Program
     }
 }
 */
+
+
+
+/*
 using System;
 
 class Program
@@ -242,3 +246,21 @@ class Program
         
     }
     };
+
+    */
+
+
+    using System;
+
+class Program
+{
+        static void Main()
+    {
+        int[] numeros = {10,20,30,40,50};
+
+        for(int i = 0; i< numeros.Length; i++)
+        {
+            Console.WriteLine(numeros[i]);
+        }
+    }
+}
