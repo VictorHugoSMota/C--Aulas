4b0e3fad8b93eeb37438a8979f08c24951bca984
Victor Hugo <victorhugo.mota26@gmail.com>
2026-02-19 17:06:10 -0300

    new calculator

--- Aulas/Program.cs
+++ Aulas/Program.cs
@@ -342,6 +342,91 @@ class Program
 */
 
 
+
+//  3° MODELO --------------------------------------------------------------------------------------  
+
+// using System;
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         int[] numeros = new int[5];
+
+//         for (int i = 0; i < numeros.Length; i++)
+//         {
+//             bool valido = false;
+
+//             while (valido == false)
+//             {
+//                 Console.Write("Digite um número: ");
+//                 valido = int.TryParse(Console.ReadLine(), out numeros[i]);
+
+//                 if (valido == false)
+//                 {
+//                     Console.WriteLine("Digite apenas números.");
+//                 }
+//             }
+//         }
+
+//         Console.WriteLine("\nValores digitados:");
+//         for (int i = 0; i < numeros.Length; i++)
+//         {
+//             Console.WriteLine("Posição " + (i + 1) + ": " + numeros[i]);
+//         }
+
+//         Console.WriteLine("\nDeseja alterar algum valor? S/N");
+//         var resposta = Console.ReadLine();
+
+//         if (resposta == "S")
+//         {
+//             int indice = 0;
+//             bool indiceValido = false;
+
+//             while (indiceValido == false)
+//             {
+//                 Console.WriteLine("Qual posição deseja alterar? (1 a 5)");
+//                 indiceValido = int.TryParse(Console.ReadLine(), out indice);
+
+//                 if (indiceValido == false || indice < 1 || indice > 5)
+//                 {
+//                     Console.WriteLine("Errado! Digite um número entre 1 e 5.");
+//                     indiceValido = false;
+//                 }
+//             }
+
+//             indice = indice - 1;
+
+//             bool novoValorValido = false;
+
+//             while (novoValorValido == false)
+//             {
+//                 Console.WriteLine("Digite o novo valor:");
+//                 novoValorValido = int.TryParse(Console.ReadLine(), out numeros[indice]);
+
+//                 if (novoValorValido == false)
+//                 {
+//                     Console.WriteLine("Erro! Digite apenas números.");
+//                 }
+//             }
+
+//             Console.WriteLine("\nValores atualizados:");
+//             for (int i = 0; i < numeros.Length; i++)
+//             {
+//                 Console.WriteLine("Posição " + (i + 1) + ": " + numeros[i]);
+//             }
+//         }
+
+//         Console.WriteLine("\nPrograma finalizado.");
+//         Console.ReadKey();
+//     }
+
+// }
+
+
+
+//  4° MODELO --------------------------------------------------------------------------------------  
+
 using System;
 
 class Program
@@ -350,65 +435,76 @@ class Program
     {
         int[] numeros = new int[5];
 
-        for (int i = 0; i < numeros.Length; i++)
+        // Entrada dos números
+        for (int i = 0; i < 5; i++)
         {
-            bool valido = false;
-
-            while (valido == false)
+            while (true)
             {
-                Console.Write("Digite um número: ");
-                valido = int.TryParse(Console.ReadLine(), out numeros[i]);
-
-                if (valido == false)
+                try
                 {
-                    Console.WriteLine("Digite apenas números.");
+                    Console.Write("Digite um número: ");
+                    numeros[i] = int.Parse(Console.ReadLine());
+                    break; // sai do while se der certo
+                }
+                catch
+                {
+                    Console.WriteLine("Digite apenas números!");
                 }
             }
         }
 
+        // Mostrar valores
         Console.WriteLine("\nValores digitados:");
-        for (int i = 0; i < numeros.Length; i++)
+        for (int i = 0; i < 5; i++)
         {
             Console.WriteLine("Posição " + (i + 1) + ": " + numeros[i]);
         }
 
-        Console.WriteLine("\nDeseja alterar algum valor? S/N");
-        var resposta = Console.ReadLine();
+        // Perguntar se quer alterar
+        Console.WriteLine("\nDeseja alterar algum valor? (S/N)");
+        string resposta = Console.ReadLine();
 
-        if (resposta == "S")
+        if (resposta.ToUpper() == "S")
         {
-            int indice = 0;
-            bool indiceValido = false;
+            int indice;
 
-            while (indiceValido == false)
+            while (true)
             {
-                Console.WriteLine("Qual posição deseja alterar? (1 a 5)");
-                indiceValido = int.TryParse(Console.ReadLine(), out indice);
-
-                if (indiceValido == false || indice < 1 || indice > 5)
+                try
                 {
-                    Console.WriteLine("Errado! Digite um número entre 1 e 5.");
-                    indiceValido = false;
+                    Console.Write("Qual posição deseja alterar? (1 a 5): ");
+                    indice = int.Parse(Console.ReadLine());
+
+                    if (indice < 1 || indice > 5)
+                    {
+                        Console.WriteLine("Escolha entre 1 e 5.");
+                        continue;
+                    }
+
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Digite apenas números!");
                 }
             }
 
-            indice = indice - 1;
-
-            bool novoValorValido = false;
-
-            while (novoValorValido == false)
+            while (true)
             {
-                Console.WriteLine("Digite o novo valor:");
-                novoValorValido = int.TryParse(Console.ReadLine(), out numeros[indice]);
-
-                if (novoValorValido == false)
+                try
                 {
-                    Console.WriteLine("Erro! Digite apenas números.");
+                    Console.Write("Digite o novo valor: ");
+                    numeros[indice - 1] = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Digite apenas números!");
                 }
             }
 
             Console.WriteLine("\nValores atualizados:");
-            for (int i = 0; i < numeros.Length; i++)
+            for (int i = 0; i < 5; i++)
             {
                 Console.WriteLine("Posição " + (i + 1) + ": " + numeros[i]);
             }
@@ -417,10 +513,7 @@ class Program
         Console.WriteLine("\nPrograma finalizado.");
         Console.ReadKey();
     }
-    
 }
-
-
 
 
 
