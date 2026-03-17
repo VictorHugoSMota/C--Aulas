449d14c0c231b455caa44ff572095c53494814e5
Victor Hugo <victorhugo.mota26@gmail.com>
2026-03-16 23:48:31 -0300

    refactor

--- Aulas/obj/Debug/net10.0/Calculadora.AssemblyInfo.cs
+++ Aulas/obj/Debug/net10.0/Calculadora.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de414aeb36686eacf60d39bfa2d28d5d36bb5d0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d409646a25b45e26aaab5b0bee8818831510c83")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
